b43472751cddd42a4e65873fb07269c13a6721e1
rjrodger <richard@ricebridge.com>
2020-05-25 16:33:45 +0100

    travis 2

---
+++ test/create.js
@@ -0,0 +1,34 @@
+
+const AWS = require("aws-sdk")
+
+var config = {
+  region: 'region',
+  endpoint: process.env.SENECA_DYNAMO_ENDPOINT
+}
+
+console.log(config)
+
+AWS.config.update(config)
+
+var ddb = new AWS.DynamoDB()
+
+ddb.createTable({
+  "TableName": "foo",
+  KeySchema: [
+    {
+      AttributeName: "id", 
+      KeyType: "HASH"
+    },
+  ],
+  AttributeDefinitions: [
+    {
+      AttributeName: "id", 
+      AttributeType: "S"
+    }, 
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5, 
+    WriteCapacityUnits: 5
+  },
+}, console.log)
+
