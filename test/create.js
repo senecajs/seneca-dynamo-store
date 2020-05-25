a98704b877cc1a8d690ae06da61a6870ea931dd5
rjrodger <richard@ricebridge.com>
2020-05-25 17:04:42 +0100

    travis 6

--- test/create.js
+++ test/create.js
@@ -15,7 +15,49 @@ AWS.config.update(config)
 var ddb = new AWS.DynamoDB()
 
 ddb.createTable({
+  "TableName": "test_foo",
+  KeySchema: [
+    {
+      AttributeName: "id", 
+      KeyType: "HASH"
+    },
+  ],
+  AttributeDefinitions: [
+    {
+      AttributeName: "id", 
+      AttributeType: "S"
+    }, 
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5, 
+    WriteCapacityUnits: 5
+  },
+}, console.log)
+
+
+ddb.createTable({
   "TableName": "foo",
+  KeySchema: [
+    {
+      AttributeName: "id", 
+      KeyType: "HASH"
+    },
+  ],
+  AttributeDefinitions: [
+    {
+      AttributeName: "id", 
+      AttributeType: "S"
+    }, 
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5, 
+    WriteCapacityUnits: 5
+  },
+}, console.log)
+
+
+ddb.createTable({
+  "TableName": "moon_bar",
   KeySchema: [
     {
       AttributeName: "id", 
