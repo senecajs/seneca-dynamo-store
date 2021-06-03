5a2eb1e6c72874f0c6fb814fd5588d1876086c69
lilsweetcaligula <efim.bogdanovsky@gmail.com>
2021-06-03 07:44:45 +0300

    Add missing table definitions

--- test/support/db/delete-test-tables.js
+++ test/support/db/delete-test-tables.js
@@ -6,6 +6,11 @@ const ddb = DynamoDb.connect({ verbose: true })
 delete_table_if_not_exists('test_foo', ddb, { verbose: true })
 delete_table_if_not_exists('foo', ddb, { verbose: true })
 delete_table_if_not_exists('moon_bar', ddb, { verbose: true })
+delete_table_if_not_exists('players', ddb, { verbose: true })
+delete_table_if_not_exists('racers', ddb, { verbose: true })
+delete_table_if_not_exists('users', ddb, { verbose: true })
+delete_table_if_not_exists('customers', ddb, { verbose: true })
+delete_table_if_not_exists('products', ddb, { verbose: true })
 
 
 function delete_table_if_not_exists(table, ddb, opts = {}) {
