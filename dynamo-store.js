059a5daaf3040dcb1c5a2f7b6aed132e84b2a343
Alex Milenkovic <jk1143298@gmail.com>
2023-05-30 20:19:46 +0200

    canon-key-ref

--- dynamo-store.js
+++ dynamo-store.js
@@ -100,6 +100,8 @@ function get_op(qv) {
 }
 
 function make_intern() {
+  const canon_ref = {}
+  
   return {
     PV: 1, // persistence version, used for data migration
 
@@ -132,17 +134,18 @@ function make_intern() {
     // TODO: seneca-entity should provide this
     entity_options: function (ent, ctx) {
       let canonkey = ent.canon$()
-
+      
       // NOTE: canonkey in options can omit empty canon parts, and zone
       // so that canonkey can match seneca.entity abbreviated canon
       let entopts =
+        canon_ref[canonkey] ||
         ctx.options.entity[canonkey] ||
         ctx.options.entity[canonkey.replace(/^-\//, '')] ||
         ctx.options.entity[canonkey.replace(/^-\/-\//, '')] ||
         ctx.options.entity[canonkey.replace(/^[^/]+\/([^/]+\/[^/]+)$/, '$1')]
 
       // TODO: use a separate cache for resolved canon ref
-      ctx.options.entity[canonkey] = ctx.options.entity[canonkey] || entopts
+      canon_ref[canonkey] = ctx.options.entity[canonkey] || entopts
 
       return entopts
     },
