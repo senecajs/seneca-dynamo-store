e5691aca121ffd89b39313087f5c3090249b304b
Alex Milenkovic <jk1143298@gmail.com>
2023-06-07 18:53:38 +0200

    refactor

--- dynamo-store.js
+++ dynamo-store.js
@@ -680,11 +680,9 @@ function make_intern() {
       if (0 < Object.keys(fq).length) {
         listreq.FilterExpression = Object.keys(cq)
           .map((k) => {
-            let cq_op = intern.build_ops(cq[k], k, 'field')
-            // console.log('CQ_OP: ', cq, cq_op)
-            return isarr(cq[k])
-              ? '(' +
-                cq[k]
+            let cq_k =  isarr(cq[k]) ? cq[k] : [ cq[k] ]
+            return '(' +
+                cq_k
                   .map((v, i) => {
                     let cq_v = intern.build_ops(v, k)
                     // console.log('cq_v: ', cq_v)
@@ -693,9 +691,6 @@ function make_intern() {
                    })
                   .join(' or ') +
                 ')'
-              : '(' + cq_op.cmps.map((c, i) =>
-                 ('#' + c.k + ` ${c.cmpop} :` + c.k + i + 'n') ).join(' and ') + ')'
-              
           })
           .join(' and ')
 
@@ -706,15 +701,12 @@ function make_intern() {
 
         listreq.ExpressionAttributeValues = Object.keys(cq).reduce(
           (a, k) => {
-            let cq_op = intern.build_ops(cq[k], k)
-            // console.log('CQ_OP: ', cq, cq_op)
+            let cq_k = isarr(cq[k]) ? cq[k] : [ cq[k] ]
             
-            isarr(cq[k])
-              ? cq[k].forEach((v, i) => {
-                  let cq_v = intern.build_ops(v, k)
-                  cq_v.cmps.forEach((c, j)=> a[':' + c.k + i + j + 'n'] = c.v )
-                })
-              : cq_op.cmps.forEach((c, i) => a[':' + c.k + i + 'n'] = c.v)
+            cq_k.forEach((v, i) => {
+              let cq_v = intern.build_ops(v, k)
+              cq_v.cmps.forEach((c, j)=> a[':' + c.k + i + j + 'n'] = c.v )
+            })
 
             return a
           },
